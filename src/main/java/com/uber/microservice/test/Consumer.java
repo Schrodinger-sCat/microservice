1717948160e9a4c93ea71657b458f1c8e47304ef
Md. Giash Uddin <giash.inument@bkash.com>
2025-12-27 12:16:36 +0600

    comment out consumer

--- src/main/java/com/uber/microservice/test/Consumer.java
+++ src/main/java/com/uber/microservice/test/Consumer.java
@@ -1,14 +1,12 @@
 package com.uber.microservice.test;
 
-import com.uber.microservice.dependency.Prescription;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class Consumer {
-    @RabbitListener(queues = "loky.prescription.queue")
-    public void receive(Prescription prescription) {
-        System.out.println("Received: " + prescription);
-  }
+//    @RabbitListener(queues = "loky.prescription.queue")
+//    public void receive(Prescription prescription) {
+//        System.out.println("Received: " + prescription);
+//  }
 
 }
