626db452c618d546388d85708ae111c8e5cfcb80
Md. Giash Uddin <giash.inument@bkash.com>
2025-12-27 12:48:36 +0600

    Received prescription

--- src/main/java/com/uber/microservice/shared/kernel/domain/Doctor.java
+++ src/main/java/com/uber/microservice/shared/kernel/domain/Doctor.java
@@ -1,5 +1,16 @@
 package com.uber.microservice.shared.kernel.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString(doNotUseGetters = true)
 public class Doctor {
   private long id;
   private String name;
